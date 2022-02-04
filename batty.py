ebb8db5c2add975ff2296c4ae361934cc224983d
Philipp Eller <peller.phys@gmail.com>
2022-02-04 16:35:55 +0100

    shared memory for results, and a few other things

--- batty.py
+++ batty.py
@@ -1,7 +1,12 @@
-from julia.api import Julia
-jl = Julia(compiled_modules=False)
-
-from julia import BAT, DensityInterface, Distributions, ValueShapes
+from julia.core import UnsupportedPythonError
+try:
+    from julia import BAT, DensityInterface, Distributions, ValueShapes, Main
+except UnsupportedPythonError:
+    import warnings
+    warnings.warn("Not able to use compiled modules, resulting in (very) slow import\n See https://pyjulia.readthedocs.io/en/latest/troubleshooting.html")
+    from julia.api import Julia
+    jl = Julia(compiled_modules=False)
+    from julia import BAT, DensityInterface, Distributions, ValueShapes, Main
 
 from collections import namedtuple
 
@@ -11,40 +16,71 @@ BAT_result = namedtuple('BAT_result', ['samples', 'weight', 'logd'])
 
 class BAT_sampler():
     
-    def __init__(self, llh, prior_specs, llh_args=(), mcalg=BAT.MetropolisHastings(), **kwargs):
+    def __init__(self, llh, prior_specs, llh_args=(), mcalg=BAT.MetropolisHastings()):
+        '''BAT_sampler Python wrapper
+        
+        Paramaters:
+        -----------
+        llh : callable
+            The likelihood function
+        prior_specs : BAT prior, or dictionary thereof
+            The prior specifications
+        llh_args : tuple
+            additional arguments to the llh function (optional)
+        mcalg : BAT MC algorithm (optional)
+
+        
+        '''
         self.llh = DensityInterface.logfuncdensity(lambda x : llh(x, *llh_args))
         self.prior_specs = prior_specs
         if isinstance(self.prior_specs, dict):
             self.prior = ValueShapes.NamedTupleDist(**prior_specs)
+            self.result_tuple = namedtuple('result_tuple', prior_specs.keys())
         else:
             self.prior = prior_specs
         self.posterior = BAT.PosteriorDensity(self.llh, self.prior)
         self.mcalg = mcalg
-        self.kwargs = kwargs
         self._result = None
-        self._result_digest = None
+        self.result = None
         
-    def run(self, nsteps=1e5):
-        self._result = BAT.bat_sample(self.posterior, BAT.MCMCSampling(mcalg=self.mcalg, nsteps=int(nsteps), **self.kwargs))
-        self._result_digest = None
-    
-    @property
-    def result(self,):
-        if self._result_digest:
-            return self._result_digest
+    def run(self, nsteps=1e5, **kwargs):
+        '''
+        Run the Sampler
         
-        if self._result is None:
-            return None
+        Paramaters:
+        -----------
+        nsteps : int
+            Number of MCMC sampling steps
+        kwargs : dict (optional)
+            additional keyword arguments to the sampler
         
+        Returns:
+        --------
+        result : namedtuple('BAT_result', ['samples', 'weight', 'logd'])
+            Containing arrays of samples, weights, and llh values
+        
+        '''
+        Main.result = BAT.bat_sample(self.posterior, BAT.MCMCSampling(mcalg=self.mcalg, nsteps=int(nsteps), **kwargs))
+        self._result = Main.result
+        
+        # define viws of arrays
         if isinstance(self.prior_specs, dict):
             samples = {}
-            for key in self.prior_dict.keys():
-                samples[key] = np.array([getattr(s.v, key) for s in self._result.result]) 
+            for key in self.prior_specs.keys():
+                if isinstance(Main.eval('result.result.v.%s[1]'%key), list):
+                    samples[key] = Main.eval('convert(Array{Float64}, reduce(hcat, result.result.v.%s))'%key)                    
+                else:
+                    samples[key] = Main.eval('convert(Array{Float64}, result.result.v.%s)'%key)
+            samples = self.result_tuple(**samples)
         else:
-            samples = np.array([s.v for s in self._result.result])
-            
-        weight = np.array([s.weight for s in self._result.result])
-        logd = np.array([s.logd for s in self._result.result])
-        self._result_digest = BAT_result(samples, weight, logd)
+            if isinstance(Main.eval('result.result.v[1]'), list):
+                samples = Main.eval('convert(Array{Float64}, reduce(hcat, result.result.v))')
+            else:
+                samples = Main.eval('convert(Array{Float64}, result.result.v)')
+                    
+        weight = Main.eval('convert(Array{Float64}, result.result.weight)')
+        logd = Main.eval('convert(Array{Float64}, result.result.logd)')      
+    
+        self.result = BAT_result(samples, weight, logd)
         
-        return  self._result_digest
+        return self.result
