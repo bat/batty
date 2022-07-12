beb6196d83a2df62885e09ae5ecf449921a5c551
Philipp Eller <peller.phys@gmail.com>
2022-07-12 12:07:49 +0200

    pip install issue / project structure fix

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="batty",
-    version="0.0.6",
+    version="0.0.7",
     packages=find_packages(),
     license="Apache 2.0",
     author="Philipp Eller",
